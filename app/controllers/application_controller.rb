b2dc7e0d5381742400f9869bf637556aaff30442
saman-zdf <samanfyz68@gmail.com>
2021-07-12 10:31:21 +0800

    added active storage table, and their association

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # devise by default redirect to home page after log in , if we need to redirect to any where else we need to set that up after_sign_in method
+  def after_sign_in_path_for(profile)
+    if (current_user.profile)
+      new_listing_path
+    else
+      new_profile_path || root_path
+    end
+  end
 end
